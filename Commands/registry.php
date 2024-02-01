7186d65e20fb84ea3304bdb42cd144ea6c07ddbb
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-02-01 20:43:35 +0900

    🔥 rm:不必要なコードの削除

--- Commands/registry.php
+++ Commands/registry.php
@@ -2,9 +2,5 @@
 return [
     Commands\Programs\Migrate::class,
     Commands\Programs\CodeGeneration::class,
-    Commands\Programs\DbWipe::class,
-    Commands\Programs\BookSearch::class,
-    Commands\Programs\GenerateCommands::class,
-    Commands\Programs\StateMigrate::class,
     Commands\Programs\Seed::class,
 ];
