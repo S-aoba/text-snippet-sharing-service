e1cff382fe8d6f5217381c4c72fc3ae2fb2108ce
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-14 22:45:27 +0900

    Add get snippet method from database

--- Helpers/DatabaseHelper.php
+++ Helpers/DatabaseHelper.php
@@ -10,7 +10,7 @@ class DatabaseHelper
     public static function getSnippet(string $hash) : array {
         $db = new MySQLWrapper();
         $hashedCode = hash('sha256', $hash);
-
+        
         $stmt = $db->prepare("SELECT * FROM snippets WHERE hashed_str = ?");
         $stmt->bind_param('s', $hashedCode);
         $stmt->execute();
