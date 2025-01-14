ab90cb7e7b9d19f94c5360cd65f128078fb3ecde
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-14 21:37:03 +0900

    Add get snippet functionally

--- Helpers/DatabaseHelper.php
+++ Helpers/DatabaseHelper.php
@@ -7,5 +7,19 @@ use Exception;
 
 class DatabaseHelper
 {
-    
- }
+    public static function getSnippet(string $hash) : array {
+        $db = new MySQLWrapper();
+        $hashedCode = hash('sha256', $hash);
+
+        $stmt = $db->prepare("SELECT * FROM snippets WHERE hashed_str = ?");
+        $stmt->bind_param('s', $hashedCode);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $snipett = $result->fetch_assoc();
+
+        if(!$snipett) throw new Exception("Could not find a single snippet in database.");
+
+        return $snipett;
+    }    
+}
--- Routing/routes.php
+++ Routing/routes.php
@@ -7,5 +7,22 @@ use Response\Render\HTMLRenderer;
 use Response\Render\JSONRenderer;
 
 return [
-    
+  // GET
+  '' => function(): HTTPRenderer {
+    return new HTMLRenderer('component/home');
+  },
+
+  'snippet' => function(): HTTPRenderer {
+    $hash = $_GET['hash'];
+
+    $hash = ValidationHelper::string($hash);
+    $snippet = DatabaseHelper::getSnippet($hash);
+
+    return new HTMLRenderer('component/snippet', ['snippet' => $snippet]);
+  },
+
+  // // POST
+  // 'create' => function(): HTTPRenderer {
+
+  // }
 ];
