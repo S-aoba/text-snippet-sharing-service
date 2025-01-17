348f554d79b1f8d6cdd720e18e35ef998664dd72
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-17 12:54:22 +0900

    Add save snippet data

--- Helpers/DatabaseHelper.php
+++ Helpers/DatabaseHelper.php
@@ -21,5 +21,21 @@ class DatabaseHelper
         if(!$snipett) throw new Exception("Could not find a single snippet in database.");
 
         return $snipett;
-    }    
+    }
+    
+    public static function saveSnippet(array $data, string $hashedCode): void
+    {
+        $db = new MySQLWrapper();
+        $snippet = $data['snippet'];
+        $password = $data['password'];
+        $syntaxHighLighting = $data['syntaxHighLighting'];
+        $pasteExpiration = $data['pasteExpiration'];
+        // TODO: $pasteExposureを入れるカラムを作成し、SQLコードの修正する
+         
+        $stmt = $db->prepare("INSERT INTO snippets (snippet, password, language, expired_at ,hashed_str) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param('sssss', $snippet, $password, $syntaxHighLighting, $pasteExpiration, $hashedCode);
+        $result = $stmt->execute();
+
+        if($result === false) throw new Exception('Could not execute the query.');
+    }
 }
